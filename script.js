2d3fe79d05830c6e003d85dabfa8ac72ebadc7cb
Nathanael Buendia <futiax@gmail.com>
2025-09-17 22:27:14 +0200

    Release 1.0.0

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+const urls = {
+    math: "https://mp2icamilleguerin.blogspot.com/",
+    info: "https://edurl.fr/mp2i-guerin",
+    physique: "https://cahier-de-prepa.fr/mp2i-guerin/",
+    lycee: "https://educonnect.education.gouv.fr/idp/profile/SAML2/Unsolicited/SSO?providerId=https%3A%2F%2Fmon.lyceeconnecte.fr%2Fauth%2Fsaml%2Fmetadata%2Fidp.xml",
+};
 document.querySelectorAll(".button-container button").forEach((btn) => {
-    const urls = {
-        math: "https://mp2icamilleguerin.blogspot.com/",
-        info: "https://edurl.fr/mp2i-guerin",
-        physique: "https://cahier-de-prepa.fr/mp2i-guerin/",
-        lycee: "https://educonnect.education.gouv.fr/idp/profile/SAML2/Unsolicited/SSO?providerId=https%3A%2F%2Fmon.lyceeconnecte.fr%2Fauth%2Fsaml%2Fmetadata%2Fidp.xml",
-    };
     const id = btn.id;
     btn.addEventListener("click", () => {
         const url = urls[id];
-        if (url) window.open(url, "_blank");
+        console.log(url);
+        window.open(url, "_blank");
     });
 });
