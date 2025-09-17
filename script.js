7840bff7216721f48c13825e4f00a7f5302367af
Nathanael Buendia <futiax@gmail.com>
2025-09-17 22:12:07 +0200

    v1.1.1

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 document.querySelectorAll(".button-container button").forEach((btn) => {
+    const urls = {
+        math: "https://mp2icamilleguerin.blogspot.com/",
+        info: "https://edurl.fr/mp2i-guerin",
+        physique: "https://cahier-de-prepa.fr/mp2i-guerin/",
+        lycee: "https://educonnect.education.gouv.fr/idp/profile/SAML2/Unsolicited/SSO?providerId=https%3A%2F%2Fmon.lyceeconnecte.fr%2Fauth%2Fsaml%2Fmetadata%2Fidp.xml",
+    };
+    const id = btn.id;
     btn.addEventListener("click", () => {
-        const url = btn.getAttribute("data-url");
+        const url = urls[id];
         if (url) window.open(url, "_blank");
     });
 });
