4704c933d37530ca58579ceb310d3268c53ed17a
Nathanael Buendia <futiax@gmail.com>
2025-09-18 21:48:18 +0200

    V1.3

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const urls = {
     info: "https://edurl.fr/mp2i-guerin",
     physique: "https://cahier-de-prepa.fr/mp2i-guerin/",
     lycee: "https://educonnect.education.gouv.fr/idp/profile/SAML2/Unsolicited/SSO?providerId=https%3A%2F%2Fmon.lyceeconnecte.fr%2Fauth%2Fsaml%2Fmetadata%2Fidp.xml",
+    franceioi: "https://www.france-ioi.org/",
+    prologins: "https://prologin.org/",
+    discord: "https://discord.gg/CDW46S5hFw",
 };
 document.querySelectorAll(".button-container button").forEach((btn) => {
     const id = btn.id;
