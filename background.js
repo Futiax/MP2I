4632f6f313b17628adadc2b1d43e1f857a6b97d1
Nathanael Buendia <futiax@gmail.com>
2026-03-17 22:59:03 +0100

    Adding weight to the background image

--- background.js
+++ background.js
@@ -23,6 +23,7 @@ for (let i = 0; i < POINTS; i++) {
         y: Math.random() * canvas.height,
         vx: Math.random() - 0.5,
         vy: Math.random() - 0.5,
+        weight: Math.random() * 0.5 + 0.75,
     });
 }
 
@@ -44,6 +45,7 @@ window.setPointsCount = function (newCount) {
                 y: Math.random() * canvas.height,
                 vx: Math.random() - 0.5,
                 vy: Math.random() - 0.5,
+                weight: Math.random() * 0.5 + 0.75,
             });
         }
     } else {
@@ -90,7 +92,7 @@ function drawGraph() {
     // Trace les points
     for (const p of points) {
         ctx.beginPath();
-        ctx.arc(p.x, p.y, 3, 0, 2 * Math.PI);
+        ctx.arc(p.x, p.y, p.weight * 2, 0, 2 * Math.PI);
         ctx.fillStyle = "#6366f1";
         ctx.fill();
     }
@@ -149,10 +151,10 @@ window.mobileCheck = function () {
     (function (a) {
         if (
             /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
-                a
+                a,
             ) ||
             /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
-                a.substr(0, 4)
+                a.substr(0, 4),
             )
         )
             check = true;
@@ -176,8 +178,8 @@ function animate() {
             const anglemouse = Math.atan2(dy, dx);
             const sqaredist = dx * dx + dy * dy || 1;
             const gravityforce = gravityStrength / sqaredist;
-            p.vx += Math.cos(anglemouse) * gravityforce;
-            p.vy += Math.sin(anglemouse) * gravityforce;
+            p.vx += (Math.cos(anglemouse) * gravityforce) / p.weight;
+            p.vy += (Math.sin(anglemouse) * gravityforce) / p.weight;
         }
         if (relative) {
             for (const astre of points) {
@@ -185,7 +187,7 @@ function animate() {
                 const dy = astre.y - p.y;
                 const sqaredist = dx * dx + dy * dy;
                 if (sqaredist === 0) continue;
-                const gravityforce = gravityStrength ** 2 / sqaredist ** 2;
+                const gravityforce = (gravityStrength ** 2 * astre.weight) / sqaredist ** 2;
                 const angle = Math.atan2(dy, dx);
                 const ax = (Math.cos(angle) * gravityforce) / Math.max(1, POINTS);
                 const ay = (Math.sin(angle) * gravityforce) / Math.max(1, POINTS);
@@ -215,14 +217,13 @@ function animate() {
             for (const other of points) {
                 if (other !== p && Math.hypot(other.x - p.x, other.y - p.y) < 8) {
                     p.vx += other.vx;
-                    p.vx *= 2;
                     p.vy += other.vy;
-                    p.vy *= 2;
+                    p.weight += other.weight;
                     points.splice(points.indexOf(other), 1);
                     document.getElementById("points-slider").value = --POINTS;
                     document.getElementById("points-value").textContent = POINTS;
                     console.log(
-                        `${points.indexOf(other)} a explosé au contact de ${points.indexOf(p)}`
+                        `${points.indexOf(other)} a explosé au contact de ${points.indexOf(p)}`,
                     );
                 }
             }
